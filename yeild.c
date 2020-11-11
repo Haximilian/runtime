24705d1326676cf92fe99b514660a149aacfd233
MaximilianCode <git@github.com:MaximilianCode>
2020-11-11 13:02:04 -0800

    remove mov from yeild

--- yeild.c
+++ yeild.c
@@ -4,10 +4,9 @@ void yeildRuntime() {
     void* kernelEntryPoint = runtimeEntryPoint;
 
     asm ("\
-        mov %1, %%rax \n\
         jmp *%0 \n\
         "
-        : "+m" (kernelEntryPoint), "+r" (runtimeStackPointer)
+        : "+m" (kernelEntryPoint)
         :
         :
     );
